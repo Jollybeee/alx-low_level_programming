2701c0315f38c531f1795b84c2fe93e020c574a0
adesola <felixadesola9@gmail.com>
2023-03-23 18:09:00 +0100

    nested loops debugging

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 - 9
+ *
+ */
+void print_numbers(void)
+{
+	int val;
+
+	for (val = 48; val <= 57; val++)
+	{
+		_putchar(val);
+	}
+	_putchar('\n');
+}
