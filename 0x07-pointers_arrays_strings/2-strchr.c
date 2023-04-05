081b343a9f4ba531fdaac96ce8d156e8e6d91b5c
adesola <felixadesola9@gmail.com>
2023-04-05 11:57:10 +0100

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * _strchr - finds a character in a strinmg
+  * @s: array to find character
+  * @c: character to find
+  * Return: a char which is a pointer
+  */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
