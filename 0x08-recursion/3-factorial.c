2f1867d6d14aa83f7497d66ea4f735ba063082d2
adesola <felixadesola9@gmail.com>
2023-04-06 02:38:51 +0100

    3-factorial.c 4-pow_recursion.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,14 +5,14 @@
  * Return: int
  */
 int factorial(int n)
-{	
+{
 	if (n == 0)
 	{
-	        return (1);
-	}	
+		return (1);
+	}
 	if (n < 0)
 	{
 		return (-1);
 	}
 	return (n * factorial(n - 1));
-}	
+}
